c3d447b5eee963d433af35cf325e80339694783b
Weigang Geng <gengwg@gmail.com>
2017-04-29 22:19:06 -0700

    add author

--- 056_merge_intervals.py
+++ 056_merge_intervals.py
@@ -4,6 +4,8 @@ Given a collection of intervals, merge all overlapping intervals.
 For example,
 Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18].
+
+Author: gengwg[at]gmail.com
 """
 
 from collections import Counter
