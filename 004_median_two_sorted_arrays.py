f3af0becddf2ab2e0031a7ffe53a58363b096847
Weigang Geng <gengwg@gmail.com>
2017-08-13 09:49:31 -0700

    4. Median of Two Sorted Arrays

---
+++ 004_median_two_sorted_arrays.py
@@ -0,0 +1,38 @@
+"""
+4. Median of Two Sorted Arrays
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+"""
+
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums3 = sorted(nums1 + nums2)
+        if len(nums3) % 2 != 0:
+            # Median is the middle number in a sorted list of numbers.
+            return nums3[len(nums3) / 2]
+        else:
+            # If there are two middle numbers, you average them.
+            return (nums3[len(nums3) / 2] + nums3[len(nums3) / 2 - 1]) / 2.0
+
+if __name__ == '__main__':
+    print Solution().findMedianSortedArrays([1, 3], [2])
+    print Solution().findMedianSortedArrays([1, 3], [2, 4])
