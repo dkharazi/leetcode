28e0900dd95dbbb1b3fd7bab69e6b649963f7aed
Weigang Geng <gengwg@gmail.com>
2017-08-14 09:31:07 -0700

    12. Integer to Roman

---
+++ 012_integer_to_roman.py
@@ -0,0 +1,28 @@
+"""
+12. Integer to Roman
+
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+"""
+
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+    # http://www.cnblogs.com/zuoyuan/p/3779581.html
+    def intToRoman(self, num):
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        list = ''
+        for i in range(0, len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                list += numerals[i]
+        return list
+
+if __name__ == '__main__':
+    print Solution().intToRoman(2)
