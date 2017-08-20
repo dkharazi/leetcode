c6b27d2d99519e5d8d8cffeb0b32841e725f7a98
Weigang Geng <gengwg@gmail.com>
2017-08-19 22:45:58 -0700

    53. Maximum Subarray

--- 053_maximum_subarray.py
+++ 053_maximum_subarray.py
@@ -13,20 +13,41 @@ More practice:
 
 https://github.com/gengwg
 """
-class Solution:
 
+
+class Solution:
     """
     https://en.wikipedia.org/wiki/Maximum_subarray_problem
     http://www.algorithmist.com/index.php/Kadane's_Algorithm
+
+    Kadane's algorithm begins with a simple inductive question:
+    if we know the maximum subarray sum ending at position i,
+    what is the maximum subarray sum ending at position i+1?
+
+    The answer turns out to be relatively straightforward:
+    either the maximum subarray sum ending at position i+1
+    includes the maximum subarray sum ending at position i as a prefix,
+    or it doesn't.
+
+    Thus, we can compute the maximum subarray sum ending at position i
+    for all positions i by iterating once over the array.
+    As we go, we simply keep track of the maximum sum we've ever seen.
     """
+
+    # http://www.cnblogs.com/zuoyuan/p/3781988.html
+    # @param A, a list of integers
+    # @return an integer
     def maxSubArray(self, A):
-        #max_ending_here = max_so_far = 0
-        #for x in A:
-        max_ending_here = max_so_far = A[0]
-        for x in A[1:]:
-            max_ending_here = max(x, max_ending_here + x)
-            max_so_far = max(max_so_far, max_ending_here)
-        return max_so_far
+        ThisSum = 0
+        MaxSum = -10000
+
+        for x in A:
+            if ThisSum < 0:
+                ThisSum = 0
+            ThisSum = ThisSum + x
+            MaxSum = max(ThisSum, MaxSum)
+
+        return MaxSum
 
     ## Brutal force solution
     def maxSubArray(self, A):
@@ -34,10 +55,29 @@ class Solution:
 
     # enumerate all sub lists...
     def sublists(self, A):
-        return [A[m:n+1] for m in range(0,len(A)+1) for n in range(m,len(A)+1)]
+        return [A[m:n + 1] for m in range(0, len(A)) for n in range(m, len(A))]
+
+    def maxSubArray(self, A):
+        # max_ending_here = max_so_far = 0
+        # for x in A:
+        max_ending_here = max_so_far = A[0]
+        for x in A[1:]:
+            # max_ending_here = max(x, max_ending_here + x)
+            # max_so_far = max(max_so_far, max_ending_here)
+            if max_ending_here > 0:
+                max_ending_here += x
+            else:
+                max_ending_here = x
+
+            if max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+
+        return max_so_far
+
 
 if __name__ == "__main__":
     # A = [-2,1,-3,4,-1,2,1,-5,4]
-    print Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
-    #print Solution().maxSubArray([-2,-1,-3,-4,-1,-2,-1,-5])
-    #print Solution().maxSubArray([2,1,3,4,1,2,1,5,4])
+    print Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    # print Solution().maxSubArray([-2,-1,-3,-4,-1,-2,-1,-5])
+    # print Solution().maxSubArray([2,1,3,4,1,2,1,5,4])
+    # print Solution().sublists([-1, 2, 3])
