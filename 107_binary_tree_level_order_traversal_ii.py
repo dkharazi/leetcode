83711cf58be1ca111ef9d470f4a83c2a784af701
Weigang Geng <gengwg@gmail.com>
2017-08-26 19:14:26 -0700

    107. Binary Tree Level Order Traversal II

---
+++ 107_binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,54 @@
+"""
+107. Binary Tree Level Order Traversal II
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values.
+(ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.preorder(root, 0, res)
+
+        # this not work, because reverse() modifies list in place, returns null.
+        # ans = res.reverse()
+        res.reverse()
+
+        # this not work, because reversed() returns list_reverseiterator not a list.
+        # return reversed(res)
+        return res
+
+    def preorder(self, root, level, res):
+        if root:
+            if len(res) < level + 1:
+                res.append([])
+            res[level].append(root.val)
+            self.preorder(root.left, level + 1, res)
+            self.preorder(root.right, level + 1, res)
