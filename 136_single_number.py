344c32459e7b3b1011d21d50a349b26780498ac2
Weigang Geng <gengwg@gmail.com>
2017-08-30 10:22:35 -0700

    solution not use reduce

--- 136_single_number.py
+++ 136_single_number.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# 136. Single Number
 #
 # Given an array of integers, every element appears twice except for one. Find that single one.
 #
@@ -5,16 +7,28 @@
 # The property of XOR.
 # x ^ x = 0
 # x ^ y ^ x = y (the order could in random arrangement)
-# Since we have only one number appear once, other number apper perfectly twice.
+# Since we have only one number appear once, other number appear perfectly twice.
 # we XOR all numbers in the array, and we would finally get the number that only appears once.
 
 import operator
 
+
 class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
+        for i in range(1, len(nums)):
+            res ^= nums[i]
+        return res
+
     def singleNumber(self, A):
         # function reduce(func, seq) continually applies the function func()
         # to the sequence seq. it returns a single value.
         return reduce(operator.xor, A)
+
 
 if __name__ == "__main__":
     print Solution().singleNumber([1, 1, 2, 3, 2, 4, 3])
