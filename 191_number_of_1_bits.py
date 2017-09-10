ac50434dd86df82f8361c951faf6fece0efb5438
Weigang Geng <gengwg@gmail.com>
2017-09-10 09:25:22 -0700

    add str count solutions

--- 191_number_of_1_bits.py
+++ 191_number_of_1_bits.py
@@ -1,3 +1,5 @@
+# 191. Number of 1 Bits
+#
 # Write a function that takes an unsigned integer and returns
 # the number of '1' bits it has (also known as the Hamming weight).
 
@@ -12,6 +14,16 @@ class Solution:
             n &= n - 1
             result += 1
         return result
+
+    def hammingWeight(self, n):
+        return bin(n).count('1')
+
+    def hammingWeight(self, n):
+        count = 0
+        for c in bin(n):
+            if c == '1':
+                count += 1
+        return count
 
 if __name__ == "__main__":
     print Solution().hammingWeight(11)
