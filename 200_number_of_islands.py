6c65efcaef1dab2c776b711f4e34dad7bf51e42d
Weigang Geng <gengwg@gmail.com>
2017-09-13 21:11:43 -0700

    200. Number of Islands

---
+++ 200_number_of_islands.py
@@ -0,0 +1,68 @@
+# 200. Number of Islands
+#
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+# You may assume all four edges of the grid are all surrounded by water.
+#
+# Example 1:
+#
+# 11110
+# 11010
+# 11000
+# 00000
+# Answer: 1
+#
+# Example 2:
+#
+# 11000
+# 11000
+# 00100
+# 00011
+# Answer: 3
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+
+        http://www.tangjikai.com/algorithms/leetcode-200-number-of-islands
+
+        We use a visited array to track whether the element is visited or not.
+        When we find a  unvisited '1', DFS to mark all surrounding '1' to visited,
+        then find other '1's.
+
+        Complexity:
+        O(mn) time
+        O(mn) space
+        """
+        m = len(grid)
+        if m == 0:
+            return 0
+        n = len(grid[0])
+
+        visited = [[False] * n for _ in range(m)]
+
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    self.dfs(i, j, grid, visited)
+                    res += 1
+        return res
+
+    def dfs(self, i, j, grid, visited):
+        visited[i][j] = True
+
+        if i + 1 < len(grid) and grid[i + 1][j] == '1' and not visited[i + 1][j]:
+            self.dfs(i + 1, j, grid, visited)
+
+        if j + 1 < len(grid[0]) and grid[i][j + 1] == '1' and not visited[i][j + 1]:
+            self.dfs(i, j + 1, grid, visited)
+
+        if i - 1 >= 0 and grid[i - 1][j] == '1' and not visited[i - 1][j]:
+            self.dfs(i - 1, j, grid, visited)
+
+        if j - 1 >= 0 and grid[i][j - 1] == '1' and not visited[i][j - 1]:
+            self.dfs(i, j - 1, grid, visited)
