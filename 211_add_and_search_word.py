ca44484bff9aea818ed6389766645ddae64c89c2
Weigang Geng <gengwg@gmail.com>
2017-09-19 22:23:53 -0700

    211. Add and Search Word - Data structure design

---
+++ 211_add_and_search_word.py
@@ -0,0 +1,91 @@
+# 211. Add and Search Word - Data structure design
+#
+# Design a data structure that supports the following two operations:
+#
+# void addWord(word)
+# bool search(word)
+#
+# search(word) can search a literal word or a regular expression string containing
+# only letters a-z or .. A . means it can represent any one letter.
+#
+# For example:
+#
+# addWord("bad")
+# addWord("dad")
+# addWord("mad")
+# search("pad") -> false
+# search("bad") -> true
+# search(".ad") -> true
+# search("b..") -> true
+# Note:
+# You may assume that all words are consist of lowercase letters a-z.
+#
+# click to show hint.
+#
+# You should be familiar with how a Trie works. If not, please work on this problem: Implement Trie (Prefix Tree) first.
+#
+# http://bookshadow.com/weblog/2015/05/16/leetcode-add-and-search-word-data-structure-design/
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+
+class WordDictionary(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        """
+        Adds a word into the data structure.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for letter in word:
+            child = node.children.get(letter)
+            if child is None:
+                child = TrieNode()
+                node.children[letter] = child
+            node = child
+        node.isWord = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the data structure.
+        A word could contain the dot character '.' to represent any one letter.
+        :type word: str
+        :rtype: bool
+        """
+        return self.find(self.root, word)
+
+    def find(self, node, word):
+        if word == '':
+            return node.isWord
+        if word[0] == '.':
+            for x in node.children:
+                if self.find(node.children[x], word[1:]):
+                    return True
+        else:
+            child = node.children.get(word[0])
+            if child:
+                return self.find(child, word[1:])
+        return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+if __name__ == '__main__':
+    obj = WordDictionary()
+    obj.addWord("bad")
+    obj.addWord("dad")
+    print obj.search("pad")
+    print obj.search(".ad")
