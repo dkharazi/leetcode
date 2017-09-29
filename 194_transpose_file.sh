9cf46dcfb82b058b3005310aea25a69fad1a6edd
Weigang Geng <gengwg@gmail.com>
2017-09-28 20:58:29 -0700

    another solution

--- 194_transpose_file.sh
+++ 194_transpose_file.sh
@@ -44,3 +44,18 @@ END {
         print s[i];
     }
 }' file.txt
+
+# 
+while read -a line; do
+    for ((i = 0; i < "${#line[@]}"; ++i)); do
+        a[$i]="${a[$i]} ${line[$i]}"
+        #echo ${line[$i]}
+        #echo ${a[i]}
+
+    done
+done < file.txt
+
+echo ------------------
+for ((i = 0; i < ${#a[@]}; ++i)); do
+    echo ${a[i]}
+done
