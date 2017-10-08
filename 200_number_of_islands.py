9ef3f1189d5e396e977b2efb86dd99443cf0e805
Weigang Geng <gengwg@gmail.com>
2017-10-08 12:47:18 -0700

    another dfs solution; immerge islands

--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -66,3 +66,35 @@ class Solution(object):
 
         if j - 1 >= 0 and grid[i][j - 1] == '1' and not visited[i][j - 1]:
             self.dfs(i, j - 1, grid, visited)
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        http://zxi.mytechroad.com/blog/searching/leetcode-200-number-of-islands/
+        for every land (1), visit its neighbor land using DFS,
+        until there is no land,
+        mark visited node as 0 (water).
+        """
+        m = len(grid)
+        if m == 0: return 0
+        n = len(grid[0])
+
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    ans += 1
+                    self.dfs(grid, i, j, m, n)
+        return ans
+
+    def dfs(self, grid, i, j, m, n):
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i + 1, j, m, n)
+        self.dfs(grid, i - 1, j, m, n)
+        self.dfs(grid, i, j + 1, m, n)
+        self.dfs(grid, i, j - 1, m, n)
