45ea6a026c3acf1ec7f0e80539f83dac22954bf9
Weigang Geng <gengwg@gmail.com>
2017-10-17 20:44:19 -0700

    online chat

---
+++ system_design/online_chat.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta
+
+class UserService(object):
+    def __init__(self):
+        self.users_by_id = {}  # key: user id, value: User
+
+    def add_user(self, user_id, name, pass_hash):
+        pass
+
+    def remove_user(self, user_id):
+        pass
+
+    def add_friend_request(self, from_user_id, to_user_id):
+        pass
+
+    def approve_friend_request(self, from_user_id, to_user_id):
+        pass
+
+    def reject_friend_request(self, from_user_id, to_user_id):
+        pass
+
+class User(object):
+    def __init__(self, user_id, name, pass_hash):
+        self.user_id = user_id
+        self.name = name
+        self.pass_hash = pass_hash
+        self.friends_by_id = {} # key: friend id, value: User
+        self.friend_ids_to_private_chats = {}   # key: friend id, value: private chats
+        self.group_chats_by_id = {} # key: chat id, value: GroupChat
+        self.received_friend_requests_by_friend_id = {} # key: friend id, value: AddRequest
+        self.sent_friend_requests_by_friend_id = {} # key: friend id, value: AddRequest
+
+
+
+
+
+
+
+
