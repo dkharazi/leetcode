115b2e72abfff5d04a35753ec860ed704d6eb573
Weigang Geng <gengwg@gmail.com>
2017-12-01 11:14:15 -0800

    remove space

--- 192_word_frequency.sh
+++ 192_word_frequency.sh
@@ -28,7 +28,7 @@
 # Read from the file words.txt and output the word frequency list to stdout.
 
 cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -nr | awk '{print $2,$1}'
-cat words.txt |  tr -s ' ' '\n' | sort | uniq -c | sort -rn | awk '{print $2" "$1}'
+cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -rn | awk '{print $2" "$1}'
 
 ## Explain:
 
