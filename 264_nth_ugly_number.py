230342930f5e239a2f7717715f30c4be5a3f1c05
Weigang Geng <gengwg@gmail.com>
2018-02-05 22:35:09 -0800

    better solution

--- 264_nth_ugly_number.py
+++ 264_nth_ugly_number.py
@@ -1,3 +1,5 @@
+# 264. Ugly Number II
+#
 # Write a program to check whether a given number is an ugly number.
 #
 # Ugly numbers are positive numbers whose prime factors only include
@@ -7,6 +9,7 @@
 # Note that 1 is typically treated as an ugly number.
 
 class Solution(object):
+    # brute force
     def isUgly(self, num):
         """
         :type num: int
@@ -41,5 +44,21 @@ class Solution(object):
 
         return number
 
+    # https://www.hrwhisper.me/leetcode-ugly-number-i-ii/
+    # https://www.youtube.com/watch?v=ZG86C_U-vRg
+    def nthUglyNumber(self, n):
+        ugly = [1] * n       # ugly[0] = 1; first ugly is 1.
+        i2 = i3 = i5 = 0    # index for candiate multiply by 2,3,5 separately
+        for i in range(1, n):
+            # find min among all candidates
+            ugly[i] = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
+            # if processed by any factor, increment it.
+            # note do not use elif: need increment all min.
+            if ugly[i] == ugly[i2] * 2:   i2 += 1
+            if ugly[i] == ugly[i3] * 3:   i3 += 1
+            if ugly[i] == ugly[i5] * 5:   i5 += 1
+        return ugly[n-1]
+
 if __name__ == "__main__":
+    print Solution().nthUglyNumber(10)
     print Solution().nthUglyNumber(1500)
