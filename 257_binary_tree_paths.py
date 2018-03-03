6ff72d3d95cc60a4123f53f4adf1cb494d29910f
Weigang Geng <gengwg@gmail.com>
2018-03-03 10:26:44 -0800

    another solution

--- 257_binary_tree_paths.py
+++ 257_binary_tree_paths.py
@@ -51,11 +51,32 @@ class Solution:
             path.pop()
 
 
+# https://gengwg.blogspot.com/2018/03/leetcode-257-binary-tree-paths.html
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+        res = []
+        if root is None:
+            return res
+        self.helper(root, '', res)
+        return res
+
+    def helper(self, node, path, result):
+        # if node is None:    # recursion stop condition
+        #    return
+        if node.left is node.right is None: # leaf node
+            result.append(path + str(node.val)) # append current found path
+        if node.left:
+            self.helper(node.left, path + str(node.val) + '->', result)
+        if node.right:
+            self.helper(node.right, path + str(node.val) + '->', result)
+
+
 if __name__ == "__main__":
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
     root.left.right = TreeNode(5)
-#    root.left.left = TreeNode(4)
-
+    root.left.left = TreeNode(4)
     print Solution().binaryTreePaths(root)
