687e34294b5553361c45be2d29f99e68a3b621b1
Weigang Geng <gengwg@gmail.com>
2018-03-03 21:17:52 -0800

    reformat

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -1,13 +1,15 @@
 # 125. Valid Palindrome
 #
-# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Given a string, determine if it is a palindrome,
+# considering only alphanumeric characters and ignoring cases.
 #
 # For example,
 # "A man, a plan, a canal: Panama" is a palindrome.
 # "race a car" is not a palindrome.
 #
 # Note:
-# Have you consider that the string might be empty? This is a good question to ask during an interview.
+# Have you consider that the string might be empty?
+# This is a good question to ask during an interview.
 #
 # For the purpose of this problem, we define empty string as valid palindrome.
 
@@ -43,6 +45,7 @@ class Solution(object):
     def isPalindrome(self, s):
         st = filter(str.isalnum, s).lower()
         return st == st[::-1]
+
 
 if __name__ == "__main__":
     print Solution().isPalindrome("")
