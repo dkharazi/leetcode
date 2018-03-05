ac45d4019d89bf13da5b5bb5148dc7ef1490bcd9
Weigang Geng <gengwg@gmail.com>
2018-03-04 20:04:15 -0800

    new solution

--- 209_min_size_subarray_sum.py
+++ 209_min_size_subarray_sum.py
@@ -1,7 +1,8 @@
 # 209. Minimum Size Subarray Sum
 #
-#  Given an array of n positive integers and a positive integer s,
-# find the minimal length of a contiguous subarray of which the sum >= s. If there isn't one, return 0 instead.
+# Given an array of n positive integers and a positive integer s,
+# find the minimal length of a contiguous subarray of which the sum >= s.
+# If there isn't one, return 0 instead.
 #
 # For example, given the array [2,3,1,2,4,3] and s = 7,
 # the subarray [4,3] has the minimal length under the problem constraint.
@@ -64,7 +65,26 @@ class Solution(object):
                 bestAns = min(bestAns, end - start)
                 sum -= nums[start]
                 start += 1
-        return [0, bestAns][bestAns <= size]
+        # return [0, bestAns][bestAns <= size]
+        # alternatively use
+        return 0 if bestAns == size+1 else bestAns
+
+    # https://gengwg.blogspot.com/2018/03/leetcode-209-minimum-size-subarray-sum.html
+    def minSubArrayLen(self, s, nums):
+        sz = len(nums)
+        minlen = sz + 1    # not possible
+        sum = 0
+        left =0            # left side of window
+        # for i, num in enumerate(nums):
+        for i in range(sz):
+            sum += nums[i]
+            # when current sum reaches s, start deleting from left
+            while left <= i and sum >= s:   # can delete left <= i. redundant?
+                # record current min length
+                minlen = min(minlen, i-left+1)
+                sum -= nums[left]
+                left += 1
+        return 0 if minlen == sz+1 else minlen
 
 
 if __name__ == '__main__':
