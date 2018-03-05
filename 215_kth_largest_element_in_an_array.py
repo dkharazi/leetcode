9b432c8c868490ff520f6aa2eba904c500ff7b37
Weigang Geng <gengwg@gmail.com>
2018-03-05 08:40:01 -0800

    add comments

--- 215_kth_largest_element_in_an_array.py
+++ 215_kth_largest_element_in_an_array.py
@@ -29,28 +29,6 @@ class Solution(object):
         # return sorted(nums)[::-1][k - 1]
         return sorted(nums, reverse=True)[k - 1]
 
-    # quick sort. O(n)
-    # https://gengwg.blogspot.com/2017/09/quickselect.html
-    def findKthLargest(self, nums, k):
-        # randomly chose a pivot within nums
-        pivot = random.choice(nums)
-        nums1, nums2 = [], []
-        # split into a pile nums1 of larger elements and nums2 of smaller elements
-        for num in nums:
-            if num > pivot:
-                nums1.append(num)
-            elif num < pivot:
-                nums2.append(num)
-            # else do nothing
-
-        if k <= len(nums1):
-            # it is in the pile of larger elements
-            return self.findKthLargest(nums1, k)
-        if k > len(nums) - len(nums2):
-            # it is in the pile of smaller elements
-            return self.findKthLargest(nums2, k - (len(nums) - len(nums2)))
-        return pivot
-
     # heapq
     def findKthLargest(self, nums, k):
         return heapq.nlargest(k, nums)[-1]
@@ -80,6 +58,35 @@ class Solution(object):
 
         nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
         return new_pivot_idx
+
+    # quick sort. O(n)
+    # https://gengwg.blogspot.com/2017/09/quickselect.html
+    def findKthLargest(self, nums, k):
+        if not nums:
+            return 0
+
+        # randomly chose a pivot within nums
+        pivot = random.choice(nums)
+        # pivot = nums[0]
+
+        # split into a pile nums1 of larger elements and nums2 of smaller elements
+        nums1, nums2 = [], []
+        for num in nums:
+            if num > pivot:
+                nums1.append(num)
+            elif num < pivot:
+                nums2.append(num)
+            # else num == pivot
+
+        if k <= len(nums1):
+            # it is in the pile of larger elements
+            return self.findKthLargest(nums1, k)
+        # else: not work because there may be piles that is equal to pivot
+        elif k > len(nums) - len(nums2):
+            # it is in the pile of smaller elements
+            return self.findKthLargest(nums2, k - (len(nums) - len(nums2)))
+        else: # it is in the pile of equal to pivot elements
+            return pivot
 
 if __name__ == '__main__':
     print Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2)
