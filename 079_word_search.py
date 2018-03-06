b51ccdd07aa7d4ae64113e6b467f98639572c309
Weigang Geng <gengwg@gmail.com>
2018-03-06 08:58:03 -0800

    shorter solution

--- 079_word_search.py
+++ 079_word_search.py
@@ -71,6 +71,29 @@ class Solution(object):
                         return True
         return False
 
+    def exist(self, board, word):
+        for i in range(len(board)):
+            for j in range((len(board[0]))):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return False
+        if board[i][j] == word[0]:
+            c = board[i][j]
+            board[i][j] = '#'
+            res = self.dfs(board, i + 1, j, word[1:]) or \
+                  self.dfs(board, i - 1, j, word[1:]) or \
+                  self.dfs(board, i, j + 1, word[1:]) or \
+                  self.dfs(board, i, j - 1, word[1:])
+            board[i][j] = c
+            return res
+        return False
+
 
 if __name__ == '__main__':
     board = [
