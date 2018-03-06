49470ba4cfe5737b629962307b202e34514aab95
Weigang Geng <gengwg@gmail.com>
2018-03-05 18:24:26 -0800

    more solutions

--- 274_hindex.py
+++ 274_hindex.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # 274. H-Index
 # 
@@ -5,7 +6,8 @@
 # write a function to compute the researcher's h-index.
 
 # According to the definition of h-index on Wikipedia: 
-# "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+# "A scientist has index h if h of his/her N papers have at least h citations each,
+# and the other N-h papers have no more than h citations each."
 
 # For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total 
 # and each of them had received 3, 0, 6, 1, 5 citations respectively. 
@@ -14,22 +16,33 @@
 
 # Note: If there are several possible values for h, the maximum one is taken as the h-index. 
 
+
 class Solution(object):
+    def hIndex(self, citations):
+        citations.sort()
+        res = 0
+        while res < len(citations) and citations[len(citations) - 1 - res] > res:
+            res += 1
+        return res
+
     # https://gengwg.blogspot.com/2018/02/re-h-index.html
     def hIndex(self, citations):
         """
         :type citations: List[int]
         :rtype: int
         """
-        if not citations:   # []
+        if not citations:  # []
             return 0
 
+        # 将其发表的所有SCI论文按被引次数从高到低排序；
         citations.sort(reverse=True)
-        for idx, citation in enumerate(citations, 1):
+        # 从前往后查找排序后的列表，直到某篇论文的序号大于该论文被引次数。所得序号减一即为H指数。
+        for idx, citation in enumerate(citations, 1):  # idx starting from 1
             if idx > citation:
                 return idx - 1
-        # all citations > idx
+        # all citations > idx. now idx == len(citations)
         return idx
+
 
 print Solution().hIndex([3, 0, 6, 1, 5])
 print Solution().hIndex([3, 2])
