426bd0a03375c0e4e190781ece2859c5288cf755
Weigang Geng <gengwg@gmail.com>
2018-03-08 14:17:19 -0800

    add comments

--- 063_unique_paths_ii.py
+++ 063_unique_paths_ii.py
@@ -35,17 +35,20 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
+        # if start is 1, no path exists
         if obstacleGrid[0][0] == 1:
             return 0
 
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
 
-        dp = [[0 for __ in range(n)] for __ in range(m)]
+        dp = [[0 for _ in range(n)] for __ in range(m)]
         dp[0][0] = 1
 
+        # first column
         for i in range(1, m):
             dp[i][0] = dp[i - 1][0] if obstacleGrid[i][0] == 0 else 0
+        # first row
         for j in range(1, n):
             dp[0][j] = dp[0][j - 1] if obstacleGrid[0][j] == 0 else 0
 
