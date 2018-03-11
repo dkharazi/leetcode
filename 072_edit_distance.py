4e4aed9d1cc7d7337f521d0ca2c8ba231261a130
Weigang Geng <gengwg@gmail.com>
2018-03-11 12:27:37 -0700

    new solution

--- 072_edit_distance.py
+++ 072_edit_distance.py
@@ -37,7 +37,7 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        m = len(word1) + 1;
+        m = len(word1) + 1
         n = len(word2) + 1
         dp = [[0 for i in range(n)] for j in range(m)]
         for i in range(n):
@@ -48,6 +48,31 @@ class Solution(object):
             for j in range(1, n):
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1,
                                dp[i - 1][j - 1] + (0 if word1[i - 1] == word2[j - 1] else 1))
+        return dp[-1][-1]
+
+    # https://gengwg.blogspot.com/2018/03/leetcode-72-edit-distance.html
+    def minDistance(self, word1, word2):
+        """
+        if word1[i-1] == word2[j-1]
+            dp[i][j] = dp[i - 1][j - 1]
+        else
+            insert: dp[i][j] = dp[i][j - 1] + 1
+            delete: dp[i][j] = dp[i - 1][j] + 1
+            replace: dp[i][j] = dp[i - 1][j - 1] + 1
+        """
+        m = len(word1) + 1
+        n = len(word2) + 1
+        dp = [[0 for _ in range(n)] for __ in range(m)]
+        for j in range(n):
+            dp[0][j] = j
+        for i in range(m):
+            dp[i][0] = i
+        for i in range(1, m):
+            for j in range(1, n):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
         return dp[-1][-1]
 
 
