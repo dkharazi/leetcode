54f908f20580f7b42ec379e5021227feada09888
Weigang Geng <gengwg@gmail.com>
2018-03-11 15:53:57 -0700

    better name variables

--- 074_search_2d_matrix.py
+++ 074_search_2d_matrix.py
@@ -26,27 +26,27 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-
-        nums = []
+        A = []
         for x in matrix:
-            nums += x
+            A += x
 
-        i = 0;
-        j = len(nums) - 1
-        while i <= j:
-            mid = (i + j) / 2
-            if nums[mid] == target:
+        n = len(A)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            mid = (r - l) / 2 + l
+            if A[mid] == target:
                 return True
-            elif nums[mid] > target:
-                j = mid - 1
+            elif A[mid] < target:
+                l = mid + 1
             else:
-                i = mid + 1
-
+                r = mid - 1
         return False
 
     # http://www.cnblogs.com/zuoyuan/p/3770061.html
     def searchMatrix(self, matrix, target):
-        i = 0;
+        i = 0
         j = len(matrix[0]) - 1
         while i < len(matrix) and j >= 0:
             if matrix[i][j] == target:
