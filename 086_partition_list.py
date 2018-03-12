2ba047688f9bcd8ea67bd5e74cd09aa081432b9d
Weigang Geng <gengwg@gmail.com>
2018-03-11 17:08:18 -0700

    remove variable p

--- 086_partition_list.py
+++ 086_partition_list.py
@@ -66,6 +66,23 @@ class Solution(object):
 
         return head1.next
 
+    # remove variable p
+    def partition(self, head, x):
+        p1 = dummy1 = ListNode(0)
+        p2 = dummy2 = ListNode(0)
+
+        while head:
+            if head.val < x:
+                p1.next = head
+                p1 = p1.next
+            else:
+                p2.next = head
+                p2 = p2.next
+            head = head.next
+        p1.next = dummy2.next
+        p2.next = None
+        return dummy1.next
+
 
 if __name__ == '__main__':
     head = ListNode(1)
