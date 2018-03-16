f7f1bd8b5a8899f9cfd4610f1e06ae75e451afa7
Weigang Geng <gengwg@gmail.com>
2018-03-15 21:32:47 -0700

    more solutions

--- 107_binary_tree_level_order_traversal_ii.py
+++ 107_binary_tree_level_order_traversal_ii.py
@@ -29,6 +29,7 @@ class TreeNode(object):
 
 
 class Solution(object):
+    # same as 102. just add reverse the result.
     def levelOrderBottom(self, root):
         """
         :type root: TreeNode
@@ -52,3 +53,40 @@ class Solution(object):
             res[level].append(root.val)
             self.preorder(root.left, level + 1, res)
             self.preorder(root.right, level + 1, res)
+
+    # not using reverse
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.preorder(root, 0, res)
+        return res
+
+    def preorder(self, root, level, res):
+        if root:
+            if len(res) <= level:
+                res.insert(0, [])
+            res[len(res)-level-1].append(root.val)
+            self.preorder(root.left, level+1, res)
+            self.preorder(root.right, level+1, res)
+
+    # use queue
+    def levelOrder(self, root):
+        res = []
+        if not root:
+            return res
+        queue = [root]  # store nodes at next level
+        while queue:
+            level = []  # store values at each level
+            for _ in range(len(queue)):
+                x = queue.pop(0)
+                level.append(x.val)
+                if x.left:
+                    queue.append(x.left)
+                if x.right:
+                    queue.append(x.right)
+            res.insert(0,level)
+        return res
+~
