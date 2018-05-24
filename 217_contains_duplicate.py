079b86d85f240c4ea897e88e4444d6dd9029ff49
Weigang Geng <gengwg@gmail.com>
2018-05-24 09:50:45 -0700

    use set

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -18,6 +18,18 @@ class Solution(object):
                 return True
             dict[num] = 1
         return False
+    # same as above, using set().
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+        for n in nums:
+            if n in s:
+                return True
+            s.add(n)
+        return False
 
     def containsDuplicate(self, nums):
         return len(nums) != len(set(nums))
