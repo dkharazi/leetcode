8da7f54a695319f5aeab8490222b694d5fdb7f2d
Weigang Geng <gengwg@gmail.com>
2018-05-26 11:21:09 -0700

    more solutions

--- 230_kth_smallest_element_bst.py
+++ 230_kth_smallest_element_bst.py
@@ -9,10 +9,11 @@
 # What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently?
 # How would you optimize the kthSmallest routine?
 
-# http://bookshadow.com/weblog/2015/07/02/leetcode-kth-smallest-element-bst/
-# in-order traversal: left->root->right
 class Solution(object):
+    # iterative
     def kthSmallest(self, root, k):
+        # in-order traversal: left->root->right
+        # http://bookshadow.com/weblog/2015/07/02/leetcode-kth-smallest-element-bst/
         stack = []
         node = root
         while node:
@@ -28,8 +29,7 @@ class Solution(object):
                 right = right.left
         return node.val
 
-# https://www.youtube.com/watch?v=CfNRc82ighw
-class Solution(object):
+    # https://www.youtube.com/watch?v=CfNRc82ighw
     def kthSmallest(self, root, k):
         # global variables
         self.res = 0
@@ -49,6 +49,28 @@ class Solution(object):
 
         dfs(root)
         return self.res
+
+    # same as above, but use separate method
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        self.res = 0
+        self.k = k
+        self.inorder(root)
+        return self.res
+
+    def inorder(self, node):
+        if not node:
+            return
+        self.inorder(node.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = node.val
+            return
+        self.inorder(node.right)
 
     # inorder traversal and put vals into a list
     def kthSmallest(self, root, k):
