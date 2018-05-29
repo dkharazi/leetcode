05b69487b4cbe51ce2fa9fb564eac451badf1697
Weigang Geng <gengwg@gmail.com>
2018-05-28 23:34:34 -0700

    add test case

--- 300_longest_increasing_subsequence.py
+++ 300_longest_increasing_subsequence.py
@@ -1,11 +1,11 @@
 # 300. Longest Increasing Subsequence
-# 
+#
 # Given an unsorted array of integers, find the length of longest increasing subsequence.
 
 # For example,
 # Given [10, 9, 2, 5, 3, 7, 101, 18],
-# The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
-# Note that there may be more than one LIS combination, 
+# The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
+# Note that there may be more than one LIS combination,
 # it is only necessary for you to return the length.
 
 # Your algorithm should run in O(n2) complexity.
@@ -30,3 +30,5 @@ class Solution(object):
         return max(dp) if dp else 0
 
 
+if __name__ == '__main__':
+    print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
