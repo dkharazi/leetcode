3ee3784b29527ac25eb21083cb16349214d83ddd
Weigang Geng <gengwg@gmail.com>
2018-07-16 21:31:14 -0700

    format

--- 364_nested_list_weight_sum_ii.py
+++ 364_nested_list_weight_sum_ii.py
@@ -4,7 +4,9 @@
 #
 # Each element is either an integer, or a list — whose elements may also be integers or other lists.
 #
-# Different from the previous question where weight is increasing from root to leaf, now the weight is defined from bottom up. i.e., the leaf level integers have weight 1, and the root level integers have the largest weight.
+# Different from the previous question where weight is increasing from root to leaf,
+# now the weight is defined from bottom up. i.e., the leaf level integers have weight 1,
+# and the root level integers have the largest weight.
 #
 # Example 1:
 # Given the list [[1,1],2,[1,1]], return 8. (four 1’s at depth 1, one 2 at depth 2)
