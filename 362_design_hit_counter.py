8f4c13592c0f4d171312e199f343506086170649
Weigang Geng <gengwg@gmail.com>
2019-05-06 22:08:22 -0700

    new solution

--- 362_design_hit_counter.py
+++ 362_design_hit_counter.py
@@ -2,9 +2,9 @@
 
 # Design a hit counter which counts the number of hits received in the past 5 minutes.
 
-# Each function accepts a timestamp parameter (in seconds granularity) 
-# and you may assume that calls are being made to the system in chronological order 
-# (ie, the timestamp is monotonically increasing). 
+# Each function accepts a timestamp parameter (in seconds granularity)
+# and you may assume that calls are being made to the system in chronological order
+# (ie, the timestamp is monotonically increasing).
 # You may assume that the earliest timestamp starts at 1.
 
 # It is possible that several hits arrive roughly at the same time.
@@ -66,6 +66,47 @@ class HitCounter:
         while self.q and timestamp - self.q[0] >= 300:
             self.q.popleft()
         return len(self.q)
+
+
+# Solution if input is out of order
+class HitCounter2(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.window = 300
+        self.times = [0 for _ in range(self.window)]
+        self.cnt = [0 for _ in range(self.window)]
+
+    def hit(self, timestamp):
+        """
+        Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity).
+        :type timestamp: int
+        :rtype: void
+        """
+        index = timestamp % self.window - 1
+
+        if self.times[index] == timestamp:
+            self.cnt[index] += 1
+        else:
+            self.times[index] = timestamp
+            self.cnt[index] = 1
+
+    def getHits(self, timestamp):
+        """
+        Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity).
+        :type timestamp: int
+        :rtype: int
+        """
+        res = 0
+        for i, time in enumerate(self.times):
+            if (timestamp - time) < 300:
+                res += self.cnt[i]
+
+        return res
 
 counter = HitCounter()
 
